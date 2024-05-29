3682e47ee10a2b30941ae4ab373326ab720f07e1
Diganta123 <diganta15-4898@diu.edu.bd>
2024-05-29 11:52:38 +0600

    Registerd models in app admin.py now can create superuser

--- jobstar/JobPortalApp/admin.py
+++ jobstar/JobPortalApp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from JobPortalApp.models import *
 
-# Register your models here.
+#username ta abstract, tai models e likhi nai 
+class job_user_display(admin.ModelAdmin):
+    list_display = ['Fullname', 'username', 'UserType']
+
+#মডেল গুলা এইখানে রেজিস্টার করেছি
+admin.site.register(Job_User, job_user_display)
