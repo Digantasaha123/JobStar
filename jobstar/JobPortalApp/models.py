b64ca893d4a4dba381ffb6acfaeba535c48ac8bc
Diganta123 <diganta15-4898@diu.edu.bd>
2024-05-29 12:00:23 +0600

    Fixed USER Varible declaration

--- jobstar/JobPortalApp/models.py
+++ jobstar/JobPortalApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class Job_User(AbstractUser):
-    [
+   USER =  [
         ('Recruiter' , 'Recruiter'),
         ('Seeker', 'Seeker')        
     ]
